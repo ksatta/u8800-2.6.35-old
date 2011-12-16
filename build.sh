52f8ff3e33f65fed974a21a5e5b06240f6df64a8
Kimmo Satta <ksatta79@gmail.com>
2011-12-16 22:21:52 +0200

    updated build scripts. username@hostname in kernel ver can be modified now

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
-#datever=`date +%d%m%Y-%H%M%S`
-#sudo hostname $hn
+datever=`date +%d%m%Y-%H%M%S`
 
 cd kernel-2.6.35-U8800pro-Gingerbread.tar
 
 #make CROSS_COMPILE=/store/android/android-toolchain-eabi-linaro.org/bin/arm-linux-androideabi- -j8
-make ARCH=arm CROSS_COMPILE=/store/android/android-ndk-r7/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/arm-linux-androideabi- -j8
+#make ARCH=arm CROSS_COMPILE=/store/android/android-ndk-r7/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/arm-linux-androideabi- -j8
+  
+# http://releases.linaro.org/11.11/components/android/toolchain/4.5/android-toolchain-eabi-linaro-4.5-2011.10-1-2011-10-21_15-21-26-linux-x86.tar.bz2
+
+U8800_COMPILE_BY="ksatta" U8800_COMPILE_HOST="$datever" make ARCH=arm CROSS_COMPILE=/store/android/android-toolchain-eabi-linaro-4.5-2011.10-1-2011-10-21_15-21-26-linux-x86/android-toolchain-eabi/bin/arm-eabi- -j8
 
 cd ..
 
+echo "datever::$datever"
 
-#echo "hostname used:$hn"
 
-#sudo hostname fujitsu
--- create_anykernel_zip.sh
+++ create_anykernel_zip.sh
@@ -1,9 +1,11 @@
+zipname=`cat kernel-2.6.35-U8800pro-Gingerbread.tar/include/generated/compile.h | grep LINUX_COMPILE_HOST | cut -d" " -f3 | tr -d \"`
+
 cp kernel-2.6.35-U8800pro-Gingerbread.tar/arch/arm/boot/zImage anykernel_zip_template/kernel/zImage
 
 cd anykernel_zip_template
 zip -r temp.zip *
 
-read -p "zip name:" zipname
+#read -p "zip name:" zipname
 
 mv temp.zip ../anykernel_zips/$zipname.zip
 
