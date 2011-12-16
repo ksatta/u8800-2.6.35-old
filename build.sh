ea7bff33304151340b6bc92e031ae3ed39235336
Kimmo Satta <ksatta79@gmail.com>
2011-12-16 21:31:58 +0200

    tweaking gitignore

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-datever=`date +%d%m%Y-%H%M%S`
+#datever=`date +%d%m%Y-%H%M%S`
 #sudo hostname $hn
 
 cd kernel-2.6.35-U8800pro-Gingerbread.tar
 
 #make CROSS_COMPILE=/store/android/android-toolchain-eabi-linaro.org/bin/arm-linux-androideabi- -j8
-make LINUX_COMPILE_BYX=ksatta LINUX_COMPILE_HOSTX=$datever ARCH=arm CROSS_COMPILE=/store/android/sourcery/bin/arm-none-eabi- -j8
+make ARCH=arm CROSS_COMPILE=/store/android/android-ndk-r7/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/arm-linux-androideabi- -j8
 
 cd ..
 
---
+++ clean.sh
@@ -0,0 +1,5 @@
+cd kernel-2.6.35-U8800pro-Gingerbread.tar
+
+make ARCH=arm CROSS_COMPILE=/store/android/android-ndk-r7/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/arm-linux-androideabi- -j8 clean
+
+cd ..
